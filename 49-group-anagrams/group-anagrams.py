8cf6710a178d97176423f18602d23651eb5c8c9d
Omar Muhammad <muhammao@mcmaster.ca>
2025-06-15 19:20:30 -0400

    Time: 19 ms (37.58%) | Memory: 22.6 MB (32.45%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = {}
+
         for word in strs:
             temp = [0] * 26
             for char in word:
-                temp[ord(char)-ord('a')] += 1
-            key = tuple(temp)
-            if key not in groups:
-                groups[key] = []
-            groups[key].append(word)
+                temp[ord(char) - ord('a')] += 1
+            temp2 = tuple(temp)
+            if temp2 in groups:
+                groups[temp2].append(word)
+            else:
+                groups[temp2] = [word]
         
         return list(groups.values())
-
         
